53eba512d54f81c66577b75f8cd012c69e963535
Fábio <fabiojas@ufba.br>
2024-06-22 18:36:43 -0300

    feat:class created

---
+++ index.js
@@ -0,0 +1,21 @@
+class Calculator {
+    constructor(currenInput,operator,previousInput ) {
+        this.currenInput = currenInput;
+        this.operator = operator;
+        this.previousInput = previousInput;
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    setOperator(operator) {
+
+    }
+
+    calculate() {
+        
+    }
+}
+
